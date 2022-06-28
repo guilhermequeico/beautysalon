007c19e3d39337a73eafed21748b99bd782a0939
guilhermequeico <gqueico@gmail.com>
2022-06-27 22:06:05 -0300

    :hammer: Change structure JS file

--- assets/js/main.js
+++ assets/js/main.js
@@ -15,16 +15,16 @@ for (const link of links) {
     })
 }
 
-const header = document.querySelector('#header')
-const navHeight = header.offsetHeight
+function changeHeaderWhenScroll() {
+    const header = document.querySelector('#header')
+    const navHeight = header.offsetHeight
 
-window.addEventListener('scroll', function() {
     if (window.scrollY >= navHeight) {
         header.classList.add('scroll')
     } else {
         header.classList.remove('scroll')
     }
-})
+}
 
 const swiper = new Swiper('.swiper', {
     slidesPerView: 1,
@@ -44,11 +44,17 @@ const scrollReveal = ScrollReveal({
 
 scrollReveal.reveal(`#home .image, #home .text, #about .image, #about .text, #services header, #services .card, #testimonials header, #testimonials .testimonials, #contact .text, #contact .links, #footer .brand, #footer .social`, {interval: 100})
 
-const backToTopButton = document.querySelector('.back-to-top')
-window.addEventListener('scroll', function() {
+function backToTop() {
+    const backToTopButton = document.querySelector('.back-to-top')
+
     if (window.scrollY >= 400) {
         backToTopButton.classList.add('show')
     } else {
         backToTopButton.classList.remove('show')
     }
+}
+
+window.addEventListener('scroll', function() {
+    changeHeaderWhenScroll()
+    backToTop()
 })
