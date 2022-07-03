308aa19369eba61bf60716a1fd7f545f892418ba
guilhermequeico <gqueico@gmail.com>
2022-07-03 05:36:25 -0300

    :iphone: Change Swiper JS breackpoints

--- assets/js/main.js
+++ assets/js/main.js
@@ -32,7 +32,13 @@ const swiper = new Swiper('.swiper', {
         el: '.swiper-pagination'
     },
     mousewheel: true,
-    keyboard: true
+    keyboard: true,
+    breakpoints: {
+        767: {
+            slidesPerView: 2,
+            setWrapperSize: true
+        }
+    }
 })
 
 const scrollReveal = ScrollReveal({
